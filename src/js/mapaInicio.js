30d994e3722cd415592f6ad19c7e65972b9f75ea
DevOld112 <XwilmerX53@gmail.com>
2023-08-21 20:12:21 -0500

    Agregando los pines de las propiedades en la pagina principal

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -4,6 +4,8 @@
     const lng = -76.9746595;
     const mapa = L.map('mapa-inicio').setView([lat, lng ], 14);
 
+    let markers = new L.FeatureGroup().addTo(mapa);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
@@ -14,12 +16,38 @@
             const respuesta = await fetch(url)
             const propiedades = await respuesta.json()
 
-            console.log(propiedades)
-        } catch (error) {
+            mostrarPropiedades(propiedades);
+
+        } 
+        
+        catch (error) {
             console.log(error)
         }
     }
 
     obtenerPropiedades()
+
+    const mostrarPropiedades = propiedades => {
+
+        propiedades.forEach(propiedad => {
+            // Agregando los pines
+
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
+                autoPan: true
+            })
+            .addTo(mapa)
+            .bindPopup(`
+                <h1 class="text-xl font-extrabold uppercase my-5">${propiedad?.titulo}</h1>
+                <img src="/uploads/${propiedad?.imagen}" alt="imagen de la propiedad ${propiedad?.titulo}">
+                <p class="font-bold text-center italic text-gray-600">${propiedad.precio.nombre} </p>
+                <p class="font-bold text-center italic text-gray-600">${propiedad.categoria.nombre} </p>
+                <a href="/propiedad/${propiedad.id}" class=" text-center font-bold block p-2 uppercase">Ver Propiedad</a>
+            `)
+
+
+            markers.addLayer(marker);
+        });
+
+    }
 
 })()
